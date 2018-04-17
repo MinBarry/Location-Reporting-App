9f618292bf7eea9f305076ef6234983acd80a9e2
MinBarry <minna.barry@gmail.com>
2018-04-16 19:14:03 -0700

    Add Google maps functionality

--- App/App/static/scripts/logScript.js
+++ App/App/static/scripts/logScript.js
@@ -1,4 +1,23 @@
+// AIzaSyBbVKx_ahhNY0PBFheQ_dH6XNssuk78zqk
+var map
+var marker
+
+// Google map initilazation
+function initMap() {
+    var uluru = { lat: -25.363, lng: 131.044 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
+    marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+    $("#map").hide()
+}
+
 $(function () {
+     // Request report list
      $.getJSON($SCRIPT_ROOT + '/api/reports',
         function (data, status_code) {
             reports = data.reports
@@ -11,16 +30,20 @@ $(function () {
                     "<td>" + report.date + "</td>"+
                     "<td>" + report.type + "</td>"+
                     "<td>" + report.description + "</td></tr>"+
-                    "<tr hidden class='reportDetail'><td colspan='6'><div class='row'>" +
-                    "<div class='col-sm'></div>" +
-                    "<div class='col-sm'></div>" +
-                    "<div class='col-sm'></div>" +
-                    "</div></td ></tr >"
-                    );
+                    "<tr hidden class='reportDetail'><div class='row'>" +
+                    "<td colspan='2'><div class='reportImage'></div></td>" +
+                    "<td colspan='2'><div class='reportMap' ></div></td>" +
+                    "<td colspan='2'><div class='reportInfo'></div></td>" +
+                    "</div></tr >"
+                );
+
             });
+            // Show report details 
             $(".singleReport").click(function () {
                 $(".reportDetail").hide()
                 $(this).next().show();
+                $(this).next().find(".reportMap").append(map.getDiv())
+                $("#map").show()
             });
         }
     );
